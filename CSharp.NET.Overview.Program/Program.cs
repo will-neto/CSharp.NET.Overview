9cd91fc95bfb4fd6448d6039101e51f828ec8442
Wilson <Wilson@DESKTOP-HOT4JS8>
2024-07-07 01:44:46 -0300

    Add project files.

---
+++ CSharp.NET.Overview.Program/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Versão com "top-level statement"
+ *  Essa funcionalidade de Execucao sem necessidade explícita da classe "Main" foi incluida na versao 9 do Csharp
+ */
+ Console.WriteLine("Execução do Program.cs com funcionalidade top-level statement (a partir versão Csharp 9)");
+
+
+/*
+ *  Até a versão 9 do CSharp, a execução era baseada através do método Main (dentro da classe Program)
+   
+    Para fins de Testes => Comentar linha 4 e remover comentários das linhas 13 a 21
+ */
+
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Execução do Program.cs através de método Main => Modelo existente deste versão 1 do Csharp");
+//    }
+//}
+
